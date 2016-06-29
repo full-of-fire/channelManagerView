de6051fb97750b18fe3b0f3b78c8f72a60ddc6a4
yj <yj@yjdemac-mini.local>
2016-06-29 18:02:57 +0800

    Initial Commit

---
+++ 频道管理视图/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  频道管理视图
+//
+//  Created by yj on 16/6/29.
+//  Copyright © 2016年 yj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
